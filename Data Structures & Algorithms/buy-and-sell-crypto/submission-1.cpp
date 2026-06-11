445fffc7fd6f1d2a80c2d13e551d5174d014c423
Abhishek Chauhan <akchauhan1172@gmail.com>
2026-06-12 02:39:22 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+
+        int maxDiff = 0;
+
+        for(int i = 0; i < prices.size() - 1; i++) {
+
+            for(int j = i + 1; j < prices.size(); j++) {
+
+                maxDiff = max(maxDiff, prices[j] - prices[i]);
+            }
+        }
+
+        return maxDiff;
+    }
+};
